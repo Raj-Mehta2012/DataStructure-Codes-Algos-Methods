5061cb3d56c7e13576cbeb0370cb7029f6132fcd
Raj Mehta <61338379+Raj-Mehta2012@users.noreply.github.com>
2020-02-29 05:07:56 -0800

    Update UserDefined_LinkedList.java

--- UserDefined_LinkedList.java
+++ UserDefined_LinkedList.java
@@ -1,6 +1,7 @@
 ***DS Linked-list (User defined)***
 
-import java.util.Scanner;
+import java.util.*;
+
 class Node 
 {
 	private int data;
@@ -29,43 +30,61 @@ class Node
 }	
 
 
-public class LLDemo {
+public class LLDemo 
+{
 		static Node head=null;
+		static Node nwnode=null;
 	
-	
-	public static void main(String[]args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter nodes: ");
-		int n=sc.nextInt();
-		System.out.println("Enter 1st Node value: ");
-		int data=sc.nextInt();
-		LLDemo obj=new LLDemo();
-		head=obj.createLL(head,data);
-		System.out.println("head: "+head);
-		for(int i=0;i<n;i++)
+		public static void main(String[]args) 
 		{
-			System.out.println("Enter value for "+(i+1)+"th node: ");
+			Scanner sc=new Scanner(System.in);
+			
+			System.out.println("Enter nodes: ");
+			int n=sc.nextInt();
+			
+			System.out.println("Enter 1st Node value: ");
+			int data=sc.nextInt();
+			
+			LLDemo obj=new LLDemo();
+			head=obj.createLL(head,data);
+			
+			System.out.println("head: "+head);
+			for(int i=1;i<n;i++)
+			{
+				System.out.println("Enter value for "+(i+1)+"th node: ");
+				data=sc.nextInt();
+				obj.createLL(head,data);
+			}
+			
+			System.out.println("Linked List Created");
+			System.out.println("Link list is :");
+			obj.printLL(head);
+			
+			System.out.println("Enter position to add node: ");
+			int pos=sc.nextInt();
+			System.out.println("Enter data: ");
 			data=sc.nextInt();
-			obj.createLL(head,data);
+			
+			nwnode=obj.createLL(nwnode,data);
+			obj.addLL(head,nwnode,pos);
+			obj.printLL(head);
 		}
-		System.out.println("Linked List Created");
-		System.out.println("Link list is :");
-		obj.printLL(head);
-		
-	}
 	
-	void printLL(Node head) {
+	void printLL(Node head) 
+	{
 		Node h=head;
 		Node p=h;
-		while(p!=null) {
+		while(p!=null) 
+		{
 			System.out.println("-->"+p.getData());
-			p.getNext();
+			p=p.getNext();
 		}
 		System.out.println();
 		
 	}
 	
-	Node createLL(Node head,int data) {
+	Node createLL(Node head,int data) 
+	{
 		Node h=head;
 		Node p=h;
 		Node q=h;
@@ -73,13 +92,36 @@ public class LLDemo {
 			h=new Node(data,null);
 		else
 		{
-			while(p!=null) {
+			while(p!=null) 
+			{
 				q=p;
-				q=p.getNext();
+				p=p.getNext();
 			}
 			p=new Node(data,null);
 			q.setNext(p);
 		}
 		return h;
 	}
+	
+	Node addLL(Node head,Node nwnode,int position) 
+	{
+		Node h=head;
+		Node p=h;
+		Node q=h;
+		int n=0;
+		
+		while(p!=null) 
+		{
+			n++;
+			if(n==position)
+				break;
+			q=p;
+			p=p.getNext();
+		}
+		nwnode.setNext(p);
+		q.setNext(nwnode);
+		
+		return h;
+	}
+	
 }
